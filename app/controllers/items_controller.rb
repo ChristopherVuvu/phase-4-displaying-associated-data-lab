237e018751d4e58bd43198dade67560705ebf06e
ChristopherVuvu <vuvumumo93@gmail.com>
2023-07-05 17:09:34 +0300

    addded

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.includes(:user)
+        render json: items_with_user(items), status: :ok
+    end
+    
+    private
+    
+    def items_with_user(items)
+        items.as_json(
+            only: [:id, :name, :description, :price],
+            include: { user: { only: [:id, :username, :city] } }
+        )
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user_with_items(user), status: :ok
+    end
+    
+    private
+    
+    def user_with_items(user)
+        {
+            id: user.id,
+            username: user.username,
+            city: user.city,
+            items: user.items.as_json(only: [:id, :name, :description, :price])
+        }
+    end
 end
